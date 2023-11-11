ec43e295ddae5f59c37f6b3442f7d7709440b7e9
DavydKod <dmmkoy@gmail.com>
2023-11-11 21:26:35 +0200

    Rename_norm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use person_parser::person_module::*;
+use person_struct_parser::person_module::*;
 
 pub fn main() {
     let _person:Person = Person{name:String::from("Ivanna"), age:25, city:String::from("Kyiv")};
